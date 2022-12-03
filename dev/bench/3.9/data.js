9bf176e88321f7028a535f561baef3865d406062
github-action-benchmark <github@users.noreply.github.com>
2022-12-03 03:01:53 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 9a9c7a20dfc3f5383b65c6bae8d7aa42c4f44e75

--- dev/bench/3.9/data.js
+++ dev/bench/3.9/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669425219004,
+  "lastUpdate": 1670036513779,
   "repoUrl": "https://github.com/hgrecco/u269b",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -108,6 +108,93 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 4.3683414292210894e-7",
             "extra": "mean: 8.734645784608842 usec\nrounds: 29499"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hernan.grecco@gmail.com",
+            "name": "Hernan",
+            "username": "hgrecco"
+          },
+          "committer": {
+            "email": "hernan.grecco@gmail.com",
+            "name": "Hernan",
+            "username": "hgrecco"
+          },
+          "distinct": true,
+          "id": "9a9c7a20dfc3f5383b65c6bae8d7aa42c4f44e75",
+          "message": "Remove invalid test now that lib functions are explicit",
+          "timestamp": "2022-12-02T23:56:44-03:00",
+          "tree_id": "4c8d8bfe405316d6f35d5164a9575b4f8c1e78a1",
+          "url": "https://github.com/hgrecco/u269b/commit/9a9c7a20dfc3f5383b65c6bae8d7aa42c4f44e75"
+        },
+        "date": 1670036513256,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_python_eval[x+y]",
+            "value": 149829.98295577086,
+            "unit": "iter/sec",
+            "range": "stddev: 4.918506149685973e-7",
+            "extra": "mean: 6.674231554142241 usec\nrounds: 20492"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_python_eval[cos(z)]",
+            "value": 123658.08793444924,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000010212567362835752",
+            "extra": "mean: 8.08681434998491 usec\nrounds: 27778"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_python_eval[x * cos(z) + y]",
+            "value": 99664.83112837341,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000006220923758247646",
+            "extra": "mean: 10.033629603123982 usec\nrounds: 26666"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_evaluate_as_string[math-x+y]",
+            "value": 141632.9674894078,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000006717295561722128",
+            "extra": "mean: 7.06050305748756 usec\nrounds: 25840"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_evaluate_as_string[math-cos(z)]",
+            "value": 162509.5615211872,
+            "unit": "iter/sec",
+            "range": "stddev: 5.618916538459087e-7",
+            "extra": "mean: 6.153484082040458 usec\nrounds: 35715"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_evaluate_as_string[math-x * cos(z) + y]",
+            "value": 60914.313137893994,
+            "unit": "iter/sec",
+            "range": "stddev: 9.686388794179432e-7",
+            "extra": "mean: 16.416502928240575 usec\nrounds: 20661"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_evaluate[math-x+y]",
+            "value": 304985.7489473761,
+            "unit": "iter/sec",
+            "range": "stddev: 2.84403673516857e-7",
+            "extra": "mean: 3.278841727691826 usec\nrounds: 61350"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_evaluate[math-cos(z)]",
+            "value": 295855.86637746124,
+            "unit": "iter/sec",
+            "range": "stddev: 3.1230820179258345e-7",
+            "extra": "mean: 3.380024240331175 usec\nrounds: 49752"
+          },
+          {
+            "name": "u269b/testsuite/benchmarks/test_basic.py::test_evaluate[math-x * cos(z) + y]",
+            "value": 121590.6918051386,
+            "unit": "iter/sec",
+            "range": "stddev: 6.22907125061177e-7",
+            "extra": "mean: 8.224313762459724 usec\nrounds: 34013"
           }
         ]
       }
